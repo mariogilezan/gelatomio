d9cf35488106e8a49e983de042a08cbba8a49932
Mario Gilezan <mariogilezan10@gmail.com>
2021-06-15 14:34:43 +0200

    hooked sanity studio with gatsby and added necessary gatsby plugins

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,10 +1,22 @@
-/**
- * Configure your Gatsby site with this file.
- *
- * See: https://www.gatsbyjs.com/docs/gatsby-config/
- */
+require("dotenv").config()
 
 module.exports = {
-  /* Your site config here */
-  plugins: [],
+  siteMetadata: {
+    title: `Gelatomio`,
+    description: `Zanatsko poslastičarska radnja "Gelato" osnovana je juna 1999. godine. Osnivač Đorđe Guga je želeo da "Gelato" bude mesto gde ćete probati najbolji italijanski sladoled proizveden u Srbiji.`,
+  },
+  plugins: [
+    `gatsby-plugin-image`,
+    `gatsby-plugin-sharp`,
+    `gatsby-transformer-sharp`,
+    {
+      resolve: `gatsby-source-sanity`,
+      options: {
+        projectId: process.env.SANITY_PROJECT_ID,
+        dataset: process.env.SANITY_DATASET,
+        // token: process.env.SANITY_TOKEN,
+        watchMode: process.env.NODE_ENV === "development",
+      },
+    },
+  ],
 }
